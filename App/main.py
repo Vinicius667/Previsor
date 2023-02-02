253f0b2fbcacbddbef987043e8b00bbeeafc286c
Vinícius Almeida <almeidabarrosdf@gmail.com>
2023-02-02 02:20:10 -0300

    update

---
+++ App/main.py
@@ -0,0 +1,14 @@
+### start imports ###
+import pandas as pd
+### end import ###
+
+### start function vars ###
+### start funcion Defined vars ###
+num = 5
+### end funcion Defined vars ###
+###
+### end function vars ###
+#### start_code ###
+num = 5
+print(num * 10)
+#### end code ###
